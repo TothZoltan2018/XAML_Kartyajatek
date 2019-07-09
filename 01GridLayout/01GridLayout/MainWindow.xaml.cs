633d914ef9fcc613312691c932788d4268d96357
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2019-07-09 14:44:54 +0200

    Gridsplitter. Hazi feladat nincs kesz.

--- 01GridLayout/01GridLayout/MainWindow.xaml.cs
+++ 01GridLayout/01GridLayout/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace _01GridLayout
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
